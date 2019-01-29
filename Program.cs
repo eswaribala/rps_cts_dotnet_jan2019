48795954124c8287938a4729d3f9ef3319eb8b1a
eswaribala <Parameswaribala@gmail.com>
2019-01-29 17:03:16 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,72 +1,67 @@
-﻿using System;
+﻿
+using AutoInsurance.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LanguageFundamentals
+namespace AutoInsurance
 {
     class Program
     {
+        static void CreateVehicle()
+        {
+            //Object for Vehicle
+            Vehicle vehicleObj = new Vehicle();
+            //setter
+            Console.WriteLine("Enter Registration No");
+            vehicleObj.RegistrationNo = Console.ReadLine();
+            Console.WriteLine("Enter Color");
+            vehicleObj.Color = Console.ReadLine();
+            Console.WriteLine("Enter Engine No");
+            vehicleObj.EngineNo = Console.ReadLine();
+            Console.WriteLine("Enter Chasis No");
+            vehicleObj.ChasisNo = Console.ReadLine();
+            Console.WriteLine("Enter Date of purchase");
+            string date = Console.ReadLine();
+            vehicleObj.DOP = Convert.ToDateTime(date);
+            Console.WriteLine("Enter Maker");
+            vehicleObj.Maker = Console.ReadLine();
+
+            //getter
+            Console.WriteLine("Registration No={0}", vehicleObj.RegistrationNo);
+            Console.WriteLine("Engine No={0}", vehicleObj.EngineNo);
+            Console.WriteLine("Chasis No={0}", vehicleObj.ChasisNo);
+            Console.WriteLine("Maker={0}", vehicleObj.Maker);
+            Console.WriteLine("DOP={0}", vehicleObj.DOP.ToLongDateString());
+        }
+
+
+        static void CreatePolicyHolder()
+        {
+            PolicyHolder policyHolder = new PolicyHolder
+            {
+                PolicyNo = 4868428,
+                FromDate = new DateTime(2009, 1, 1),
+                ToDate = new DateTime(2020, 1, 1),
+                Address = "Chennai",
+                DOB = new DateTime(1970, 2, 12),
+                Email = "Param@gmail.com",
+                GenderType = Gender.FEMALE,
+                NameInsured = "Parameswari",
+                PhoneNo = 39694365634
+            };
+        }
+
         static void Main(string[] args)
         {
-            ReverseString();
-            Console.ReadLine();
-        }
-
-
-        static void ReverseString()
-        {
-            string userName = "Parameswari";
+            CreateVehicle();
+            CreatePolicyHolder();
             
-            //reverse the string
-            char[] userNameArray = userName.ToCharArray();
-            
-            int length = userNameArray.Length-1;
-            Console.WriteLine(length);
-            //Console.WriteLine(userNameArray[length - 1]);
-            //Console.WriteLine(userNameArray[length - 2]);
-           // Console.WriteLine(userNameArray[length - 3]);
-           // Console.WriteLine(userNameArray[length - 4]);
-
-          //method1
-           for (int i=userNameArray.Length-1;i>=0;i--)
-                Console.Write(userNameArray[i]);
-
-            Console.WriteLine("\n");
-            //method 2
-            Array.Reverse(userNameArray);
-            foreach(char value in userNameArray)
-                   Console.Write(value);
-
-            //method 3
-            length = userNameArray.Length - 1;
-            Console.WriteLine();
-            //entry controlled loop
-            while(length>=0)
-            {
-
-                Console.Write(userNameArray[length]);
-                length--;
-
-            }
-            //method 4
-            //exit controlled loop
-            Console.WriteLine();
-            length = userNameArray.Length-1;
-            do
-            {
-                Console.Write(userNameArray[length]);
-                length--;
-
-            }
-            while (length >= 0);
-
-            
-            
+            Console.Read();
 
 
         }
-
     }
 }
